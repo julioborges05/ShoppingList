aff9406d1b5e0e609345489934c0c5ec685c35ab
Júlio Borges <julioborges05@hotmail.com>
2025-04-12 20:17:49 -0300

    Initial commit

---
+++ src/test/java/com/julionborges/lita_de_compras/ListaDeComprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julionborges.lita_de_compras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeComprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
